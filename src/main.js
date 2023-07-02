1fc69ab664cb9c456b7443685d472c806bf2b0d0
巴巴罗萨_Rotbart <1016012907@qq.com>
2023-07-02 14:20:49 +0800

    axios封装

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import router from './router'
 
 const app = createApp(App)
 
+// 测试接口函数
+import { getCategory } from '@/apis/testApi'
+getCategory().then( res => {
+    console.log(res);
+} )
+
 app.use(createPinia())
 app.use(router)
 
