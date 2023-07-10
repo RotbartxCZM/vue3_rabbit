41d7ebbbed4464ea1d13634ac441c89bd9fefd75
巴巴罗萨_Rotbart <1016012907@qq.com>
2023-07-10 21:44:23 +0800

    详情页部分实现

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // 配置path和component对应关系
@@ -14,7 +15,8 @@ const router = createRouter({
     { path: '/', component: Layout, children: [
       { path:'', component: Home},
       { path:'category/:id', component: Category},
-      { path:'category/sub/:id', component: SubCategory}
+      { path:'category/sub/:id', component: SubCategory},
+      { path:'detail/:id', component: Detail}
     ]},
     { path: '/login', component: Login }
   ],
