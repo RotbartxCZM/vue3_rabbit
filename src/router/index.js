030a959f508fd625a0c03c9b3636f8a52bc873b6
巴巴罗萨_Rotbart <1016012907@qq.com>
2023-07-15 22:39:56 +0800

    完成了订单页

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,8 @@ import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
-
+import CheckOut from '@/views/CheckOut/index.vue'
+import Pay from '@/views/Pay/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // 配置path和component对应关系
@@ -19,7 +20,9 @@ const router = createRouter({
       { path:'category/:id', component: Category},
       { path:'category/sub/:id', component: SubCategory},
       { path:'detail/:id', component: Detail},
-      { path: 'cartlist', component: CartList}
+      { path: 'cartlist', component: CartList},
+      { path: 'checkout', component: CheckOut},
+      { path: 'pay', component: Pay}
     ]},
     { path: '/login', component: Login }
   ],
